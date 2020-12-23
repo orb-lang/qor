e0aeba4fb4851d582dbb4c0a6710cc043e06d642
Daniel Slomovits <daniels220@gmail.com>
2020-12-23 12:38:48 -0500

    Assert that lower <= upper in Math.clamp
    There's not really an unambiguously right answer to clamp(<n>, 1, -2)—the old implementation returned -2, which is probably a *worse* answer than 1 would be, but either way is going to be wrong sometimes. Better to force the caller to really consider what they want.

--- src/core/math.lua
+++ src/core/math.lua
@@ -27,14 +27,19 @@ end
 
 
 
+
+local assertfmt = assert(require "core:_base" . assertfmt)
 function Math.clamp(value, lower, upper)
-  if lower and value < lower then
-    value = lower
-  end
-  if upper and value > upper then
-    value = upper
-  end
-  return value
+   if lower and upper then
+      assertfmt(lower <= upper, "Clamp range must be nonempty (lower <= upper), got (%d, %d)", lower, upper)
+   end
+   if lower and value < lower then
+      value = lower
+   end
+   if upper and value > upper then
+      value = upper
+   end
+   return value
 end
 
 
