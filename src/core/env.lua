70b060e4738822d8e63fc2851e287bd050f60b8b
Sam Atman <atmanistan@gmail.com>
2021-09-24 10:26:37 -0700

    Add Env

---
+++ src/core/env.lua
@@ -0,0 +1,25 @@
+
+
+
+
+
+
+
+
+local Env = {}
+function Env.fenv(...)
+   local _env = {}
+   local f = unpack(...)
+   if not f then return _env end
+   if f then
+      for k,v in next, f, nil do
+         _env[k] = v
+      end
+      return _env
+   end
+end
+
+
+
+return Env
+
