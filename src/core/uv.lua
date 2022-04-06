733f7e2c6624cdefc013ed0426d0c85d0fdb5cd5
Sam Atman <atmanistan@gmail.com>
2022-04-06 11:16:34 -0400

    Replaces linepos algorithm with something simpler
    This builds up only as much cache as it needs, and then destroys
    it.
    
    Now we need to detect whether we're searching forward or backward

--- src/core/uv.lua
+++ src/core/uv.lua
@@ -5,11 +5,25 @@
 
 
 
+
+
+
+
+
+
+
+
+
+
+
 local assert = assert or error "no assert"
 
 local Uv = {}
 
 local uv = assert(require "luv")
+
+local s = require "status:status" ()
+s.chatty = true
 
 
 
@@ -32,6 +46,32 @@ function Uv.deferby(event, ms)
    timer:start(ms, 0, _event)
 
    return;
+end
+
+
+
+
+
+
+
+
+
+
+
+local resume = assert(coroutine.resume)
+
+function Uv.once(event)
+   local idle = uv.new_idle()
+   idle:start(function()
+      idle:stop()
+      idle:close()
+      s:chat("executing event of type %s", type(event))
+      if type(event) == 'thread' then
+         resume(event)
+      else
+         event()
+      end
+   end)
 end
 
 
